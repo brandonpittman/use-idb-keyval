59bac5863e9ab4484816aaf111046de5896a4be1
Brandon Pittman <blp@hey.com>
2021-02-24 14:34:06 +0900

    feat: let setter take a function
    passes along the previous value

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { get, set } from "./idb.js";
 
 export default function useIdbKeyval(key, initialState) {
   const [item, setItem] = useState(initialState);
+
   useEffect(() => {
     get(key).then((value) => value === undefined || setItem(value));
   }, [key]);
@@ -11,8 +12,17 @@ export default function useIdbKeyval(key, initialState) {
   return [
     item,
     (value) => {
-      setItem(value);
-      return set(key, value);
+      if (typeof value === "function") {
+        let prevValue;
+        setItem((prev) => {
+          prevValue = prev;
+          value(prev);
+        });
+        return set(key, value(prevValue));
+      } else {
+        setItem(value);
+        return set(key, value);
+      }
     },
   ];
 }
