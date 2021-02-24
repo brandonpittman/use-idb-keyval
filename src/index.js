5daf69785ece876cda3cd37aec2a861a4d5dba78
Brandon Pittman <blp@hey.com>
2021-02-24 18:29:14 +0900

    feat: add reset method
    third value in returned array

--- src/index.js
+++ src/index.js
@@ -3,29 +3,30 @@ import { useEffect, useState } from "react";
 import { get, set } from "./idb.js";
 
 export default function useIdbKeyval(key, initialState) {
-  //const [item, setItem] = useState(initialState);
+  const [item, setItem] = useState(initialState);
 
-  // useEffect(() => {
-  //   if (typeof window !== "undefined")
-  //     get(key).then((value) => value === undefined || setItem(value));
-  // }, [key]);
+  useEffect(() => {
+    if (typeof window !== "undefined")
+      get(key).then((value) => value === undefined || setItem(value));
+  }, [key]);
 
   return [
-    true,
+    item,
+    (value) => {
+      if (typeof value === "function") {
+        setItem((prev) => {
+          const prevValue = value(prev);
+          set(key, prevValue);
+          return prevValue;
+        });
+      } else {
+        setItem(value);
+        set(key, value);
+      }
+    },
     () => {
-      // if (typeof value === "function") {
-      //   let prevValue;
-      //   setItem((prev) => {
-      //     prevValue = prev;
-      //     value(prev);
-      //   });
-      //   return set(key, value(prevValue));
-      // } else {
-      //   setItem(value);
-      //   return set(key, value);
-      // }
+      setItem(initialState);
+      set(key, initialState);
     },
   ];
 }
-
-// export { get, set };
