32d4c281e9d4b9508219a32a05207c3d8bcff7f6
Brandon Pittman <brandon@blp.is>
2021-07-16 15:25:49 +0900

    fix: 🐛 typings

--- src/index.ts
+++ src/index.ts
@@ -2,46 +2,50 @@ import { useEffect, useCallback, useState } from 'react';
 
 import { get, set } from './idb';
 
-const useIdbKeyval = (key: string, initialState: any, initFn: any) => {
-	const [item, setItem] = useState(initialState);
+const useIdbKeyval = (
+  key: string,
+  initialState: unknown,
+  initFn: (initialState: unknown) => unknown
+) => {
+  const [item, setItem] = useState(initialState);
 
-	useEffect(() => {
-		(async () => {
-			if (typeof window !== 'undefined') {
-				const value = await get(key);
-				if (value) {
-					setItem(value);
-				} else {
-					const initFnResult = initFn ? initFn(initialState) : null;
-					setItem(initFnResult ? initFnResult : initialState);
-					set(key, initFnResult ? initFnResult : initialState);
-				}
-			}
-		})();
-	}, [key]);
+  useEffect(() => {
+    (async () => {
+      if (typeof window !== 'undefined') {
+        const value = await get(key);
+        if (value) {
+          setItem(value);
+        } else {
+          const initFnResult = initFn ? initFn(initialState) : null;
+          setItem(initFnResult ? initFnResult : initialState);
+          set(key, initFnResult ? initFnResult : initialState);
+        }
+      }
+    })();
+  }, [key]);
 
-	return [
-		item,
-		useCallback(
-			value => {
-				if (typeof value === 'function') {
-					setItem((prev: typeof item) => {
-						const prevValue = value(prev);
-						set(key, prevValue);
-						return prevValue;
-					});
-				} else {
-					setItem(value);
-					set(key, value);
-				}
-			},
-			[set, setItem]
-		),
-		useCallback(() => {
-			setItem(initialState);
-			set(key, initialState);
-		}, [set, setItem]),
-	];
+  return [
+    item,
+    useCallback(
+      value => {
+        if (typeof value === 'function') {
+          setItem((prev: typeof item) => {
+            const prevValue = value(prev);
+            set(key, prevValue);
+            return prevValue;
+          });
+        } else {
+          setItem(value);
+          set(key, value);
+        }
+      },
+      [set, setItem]
+    ),
+    useCallback(() => {
+      setItem(initialState);
+      set(key, initialState);
+    }, [set, setItem]),
+  ];
 };
 
 export { useIdbKeyval, get, set };
