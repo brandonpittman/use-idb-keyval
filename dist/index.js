80b59338112ced3634257e8ee0c27a511f64aee3
Brandon Pittman <brandon@blp.is>
2021-12-09 11:07:07 +0900

    update deps

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./use-idb-keyval.cjs.production.min.js')
-} else {
-  module.exports = require('./use-idb-keyval.cjs.development.js')
-}
